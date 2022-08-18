d3d01fb5244e5bda966a1b4f2ce9ea0a79d539ff
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-08-18 17:04:45 -0300

    modal structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
 import logoImg from '../../assets/logo.svg';
 
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
@@ -8,7 +10,21 @@ const Header: React.FC = () => {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
