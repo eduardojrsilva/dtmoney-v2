6950941ed69accab78a2e047b0979fa53a49ab19
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-08-19 14:59:06 -0300

    summary hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -5,22 +5,10 @@ import { TransactionsContext } from '../../contexts/TransactionsContext';
 import { priceFormatter } from '../../utils/formatter';
 
 import { SummaryCard, SummaryContainer } from "./styles";
+import { useSummary } from '../../hooks/useSummary';
 
 const Summary: React.FC = () => {
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce((acc, transaction) => {
-    if (transaction.type === 'income') {
-      acc.income += transaction.price;
-      acc.total += transaction.price;
-
-    } else {
-      acc.outcome += transaction.price;
-      acc.total -= transaction.price;
-    }
-
-    return acc;
-  }, { income: 0, outcome: 0, total: 0 });
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
