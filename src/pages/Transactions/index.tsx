fb06342d38a2556cad4a79a39958a7f6dc64debc
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-08-19 14:51:04 -0300

    formatting values

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 import Header from "../../components/Header";
 import Summary from "../../components/Summary";
@@ -35,11 +36,12 @@ const Transactions: React.FC = () => {
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHighlight $variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
                   </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
             ))}
           </tbody>
